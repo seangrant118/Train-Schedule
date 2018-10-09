bd5f49915a771c52a3744aa50e79b1c7dec87382
Sean Grant <seangrant118@gmail.com>
2018-10-09 06:54:45 -0400

    saving

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCkQEOUwR30Mm7wbF4AtFPe0yw06Wyo96M",
+    authDomain: "class-activities-b5bcc.firebaseapp.com",
+    databaseURL: "https://class-activities-b5bcc.firebaseio.com",
+    projectId: "class-activities-b5bcc",
+    storageBucket: "class-activities-b5bcc.appspot.com",
+    messagingSenderId: "197797132539"
+  };
+  firebase.initializeApp(config);
+
+  
+})
